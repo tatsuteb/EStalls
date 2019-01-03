3151e0430feb7a8406a4daaa4ae639c4c2677bc9
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2019-01-04 02:41:50 +0900

    フロントから作品を投稿できるようにする

--- EStalls/Areas/Seller/Pages/Items/Register.cshtml.cs
+++ EStalls/Areas/Seller/Pages/Items/Register.cshtml.cs
@@ -1,17 +1,115 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using EStalls.Data.Models;
+using EStalls.Services.Items;
+using EStalls.ValidationAttributes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace EStalls.Areas.Seller.Pages.Items
 {
     [Area("Seller")]
     public class RegisterModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IItemService _itemService;
+        private readonly ILogger<RegisterModel> _logger;
+
+        public RegisterModel(
+            UserManager<AppUser> userManager,
+            IItemService itemService,
+            ILogger<RegisterModel> logger)
         {
+            _userManager = userManager;
+            _itemService = itemService;
+            _logger = logger;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [StringLength(100, ErrorMessage = "{0}は{1}文字以下です")]
+            [Display(Name = "タイトル")]
+            public string Title { get; set; }
+
+            [Required]
+            [StringLength(500, ErrorMessage = "{0}は{1}文字以下です")]
+            [Display(Name = "説明")]
+            public string Description { get; set; }
+
+            [Required]
+            [MinValue(500, ErrorMessage = "{0}は{1}円以上です")]
+            [Display(Name = "価格（円）")]
+            public int Price { get; set; }
+
+            [Required]
+            [Display(Name = "プレビュー用ファイル")]
+            public List<IFormFile> PreviewFiles { get; set; }
+
+            [Required]
+            [Display(Name = "サムネール用ファイル")]
+            public IFormFile ThumbnailFile { get; set; }
+
+
+            [Required]
+            [StringLength(50, ErrorMessage = "{0}は{1}文字以下です")]
+            [Display(Name = "バージョン")]
+            public string Version { get; set; }
+
+            [Required]
+            [Display(Name = "販売データファイル")]
+            public List<IFormFile> DlFiles { get; set; }
+        }
+
+        public void OnGet(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _itemService.SaveItemAsync(new InputItemModel()
+                    {
+                        Title = Input.Title,
+                        Description = Input.Description,
+                        Price = Input.Price,
+                        Version = Input.Version,
+                        ThumbnailFile = Input.ThumbnailFile,
+                        PreviewFiles = Input.PreviewFiles,
+                        DlFiles = Input.DlFiles
+                    });
+                }
+                catch (Exception e)
+                {
+                    _logger.LogWarning(e.Message);
+
+                    return LocalRedirect(returnUrl);
+                }
+            }
+
+            StatusMessage = "作品が投稿されました";
+            return RedirectToPage();
         }
     }
 }
---
+++ EStalls/ValidationAttributes/MinValueAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace EStalls.ValidationAttributes
+{
+    public class MinValueAttribute : ValidationAttribute, IClientModelValidator
+    {
+        private readonly double _minValue;
+
+        public MinValueAttribute(double minValue)
+        {
+            this._minValue = minValue;
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            if (context == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            MergeAttribute(context.Attributes, "data-val", "true");
+            MergeAttribute(context.Attributes, "data-val-valuegreaterthan", this.FormatErrorMessage(context.ModelMetadata.DisplayName ?? context.ModelMetadata.PropertyName));
+            MergeAttribute(context.Attributes, "data-val-valuegreaterthan-min", this._minValue.ToString());
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format(CultureInfo.CurrentCulture, ErrorMessageString, name, this._minValue);
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!double.TryParse(value.ToString(), out var inputValue))
+                return new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
+
+            return inputValue >= this._minValue
+                ? ValidationResult.Success
+                : new ValidationResult(this.FormatErrorMessage(validationContext.DisplayName));
+        }
+
+        private static bool MergeAttribute(IDictionary<string, string> attributes, string key, string value)
+        {
+            if (attributes.ContainsKey(key))
+            {
+                return false;
+            }
+
+            attributes.Add(key, value);
+            return true;
+        }
+    }
+}
