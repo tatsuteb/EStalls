5fafdf9e9f55fe88f1ecc92e8e4c07f349e8b965
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2019-01-03 21:47:59 +0900

    販売者エリアを用意 作品投稿用ページを用意

---
+++ EStalls/Areas/Seller/Pages/Items/Register.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EStalls.Areas.Seller.Pages.Items
+{
+    [Area("Seller")]
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
