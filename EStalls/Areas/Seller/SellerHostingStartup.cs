ec125e5f6cfde7b792c8f9f2de47d56b9a3dc081
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2019-01-04 02:40:08 +0900

    Sellerエリア用スタートアップを追加 メッセージ表示パーシャルを追加 タグヘルパーの追加

---
+++ EStalls/Areas/Seller/SellerHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EStalls.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EStalls.Areas.Seller.SellerHostingStartup))]
+namespace EStalls.Areas.Seller
+{
+    public class SellerHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
