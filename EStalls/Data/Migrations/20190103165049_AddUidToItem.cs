adaedc0121afa12a950a21c6d6d001346e86f14b
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2019-01-04 02:37:32 +0900

    ItemテーブルにUidとサムネールファイル名カラムを追加

---
+++ EStalls/Data/Migrations/20190103165049_AddUidToItem.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EStalls.Data.Migrations
+{
+    public partial class AddUidToItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Uid",
+                table: "Item",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Uid",
+                table: "Item");
+        }
+    }
+}
---
+++ EStalls/Data/Migrations/20190103172520_AddThumbnailFileNameToItem.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EStalls.Data.Migrations
+{
+    public partial class AddThumbnailFileNameToItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailFileName",
+                table: "Item",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ThumbnailFileName",
+                table: "Item");
+        }
+    }
+}
