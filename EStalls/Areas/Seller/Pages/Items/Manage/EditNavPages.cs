516c6ab88c5be37ee10c9fa30e5231126ed3ae57
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2019-01-06 15:08:46 +0900

    作品の基本情報編集ページとバージョン管理ページ用レイアウトを作成 各ページのリンクにItemIdを渡せるように、専用のページモデルを用意

---
+++ EStalls/Areas/Seller/Pages/Items/Manage/EditNavPages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EStalls.Areas.Seller.Pages.Items.Manage
+{
+    public class EditNavPages
+    {
+        public static string EditBaseInfo => "EditBaseInfo";
+        public static string EditVersion => "EditVersion";
+
+        public static string EditBaseInfoNavClass(ViewContext viewContext) => PageNavClass(viewContext, EditBaseInfo);
+        public static string EditVersionNavClass(ViewContext viewContext) => PageNavClass(viewContext, EditVersion);
+
+        private static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                             ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+    }
+}
---
+++ EStalls/Areas/Seller/Pages/Items/Manage/EditPageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EStalls.Areas.Seller.Pages.Items.Manage
+{
+    public class EditPageModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public Guid ItemId { get; set; }
+    }
+}
