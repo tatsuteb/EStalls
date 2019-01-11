1cdfc391ff521bf670d82dd64402fab8a426f3d8
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2019-01-12 01:46:12 +0900

    作品をカートに入れる機能を実装

--- EStalls/Pages/Item/Index.cshtml.cs
+++ EStalls/Pages/Item/Index.cshtml.cs
@@ -2,7 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EStalls.Data;
 using EStalls.Data.Interfaces;
+using EStalls.Data.Models;
+using EStalls.Utilities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,12 +15,25 @@ namespace EStalls.Pages.Item
 {
     public class IndexModel : PageModel
     {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
         private readonly IItemService _itemService;
+        private readonly ICartService _cartService;
+        private readonly ICartItemService _cartItemService;
+
 
         public IndexModel(
-            IItemService itemService)
+            SignInManager<AppUser> signInManager,
+            UserManager<AppUser> userManager,
+            IItemService itemService,
+            ICartService cartService,
+            ICartItemService cartItemService)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
             _itemService = itemService;
+            _cartService = cartService;
+            _cartItemService = cartItemService;
         }
 
         [BindProperty(SupportsGet = true)]
@@ -26,6 +44,36 @@ namespace EStalls.Pages.Item
         public async Task OnGetAsync()
         {
             Item = await _itemService.GetAsync(ItemId);
+        }
+
+        public async Task<IActionResult> OnPostAddCartAsync()
+        {
+            // セッションからカートIDを取得
+            var cartId = HttpContext.Session.Get<Guid>("_CartId");
+
+            if (cartId == Guid.Empty)
+            {
+                cartId = Guid.NewGuid();
+                HttpContext.Session.Set(Constants.SessionKeys.CartId, cartId);
+            }
+
+            var hasItem = _cartItemService.GetAll()
+                .Where(x => x.CartId == cartId)
+                .Where(x => x.ItemId == ItemId)
+                .Select(x => x.ItemId)
+                .Contains(ItemId);
+
+            if (!hasItem)
+            {
+                await _cartItemService.AddAsync(new CartItem()
+                {
+                    CartId = cartId,
+                    ItemId = ItemId
+                });
+            }
+
+
+            return RedirectToPage();
         }
     }
 }
