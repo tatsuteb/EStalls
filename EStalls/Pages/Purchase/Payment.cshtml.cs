5558a5b393a78f3f9882cbfbb78fdba90daf8a41
DESKTOP-B7036PK\tatsuteb <tatsuteb@gmail.com>
2019-01-16 20:55:18 +0900

    エラーで戻ってきたときのアクションを用意 トークンを受け取って、確認ページへ遷移するアクションを用意

--- EStalls/Pages/Purchase/Payment.cshtml.cs
+++ EStalls/Pages/Purchase/Payment.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EStalls.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,38 @@ namespace EStalls.Pages.Purchase
 {
     public class PaymentModel : PageModel
     {
+        [TempData]
+        public string StatusMessage { get; set; }
+
         public void OnGet()
         {
         }
+
+        /// <summary>
+        /// 別のページからエラーで帰ってきたとき用
+        /// </summary>
+        /// <param name="statusMessage"></param>
+        /// <returns></returns>
+        public IActionResult OnGetErrorReturn(string statusMessage)
+        {
+            StatusMessage = statusMessage;
+
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPost(string ccToken)
+        {
+            if (string.IsNullOrWhiteSpace(ccToken))
+            {
+                StatusMessage = "カード情報が不正です。";
+                return RedirectToPage();
+            }
+
+            // TODO: 実際はUidをキーに含めて、Redis等に一時保存する
+            HttpContext.Session.Set(Constants.SessionKeys.CcToken, ccToken);
+
+            return RedirectToPage("/Purchase/Confirm");
+        }
+
     }
 }
